62250d0faee7376e45cf9b9b82db4fa036341505
Tim De Pauw <timdp@users.noreply.github.com>
2019-06-16 11:55:21 +0200

    Bump dependencies; require Node 12

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill'
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
