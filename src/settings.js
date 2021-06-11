380fd61342fc0128d5d110b8d6cf001a8feb18ed
Tim De Pauw <tim.depauw@doubleverify.com>
2021-06-11 11:48:44 +0200

    Remove unused host-toggling logic

--- src/settings.js
+++ src/settings.js
@@ -1,41 +1,11 @@
 const { protocol, host } = window.location
 
-const isDev = process.env.NODE_ENV === 'development'
-const port = isDev ? process.env.PORT || 3000 : null
-
-const S3_REGION = 'us-west-2'
-const PUBLIC_HOST_1 = 'vasttester.iabtechlab.com'
-const PUBLIC_HOST_2 = 'vasttester2.iabtechlab.com'
-const DEV_HOST_1 = `localhost:${port}`
-const DEV_HOST_2 = `127.0.0.1:${port}`
-
-let altHost
-if (isDev) {
-  altHost = host === DEV_HOST_2 ? DEV_HOST_1 : DEV_HOST_2
-} else {
-  if (host === PUBLIC_HOST_2) {
-    altHost = PUBLIC_HOST_1
-  } else {
-    const match = /^vasttester-(?:origin-)?(\w+)\.iabtechlab\.com(\.s3[^.]*\.amazonaws\.com)?$/.exec(
-      host
-    )
-    if (match != null) {
-      const [, branch, s3] = match
-      const suffix = s3 == null ? `.s3-website-${S3_REGION}.amazonaws.com` : ''
-      altHost = `vasttester-origin-${branch}.iabtechlab.com` + suffix
-    } else {
-      altHost = PUBLIC_HOST_2
-    }
-  }
-}
-
 export const APP_NAME = process.env.REACT_APP_NAME
 export const APP_VENDOR = process.env.REACT_APP_VENDOR
 export const APP_VERSION = process.env.REACT_APP_VERSION
 export const APP_BRANCH = process.env.REACT_APP_BRANCH
 export const APP_BUILD_DATE = process.env.REACT_APP_BUILD_DATE
 export const APP_URL = `${protocol}//${host}`
-export const APP_URL_ALT = `${protocol}//${altHost}`
 
 export const DEFAULT_VAST_URL = `${APP_URL}/fixtures/vast/vast3.xml`
 
